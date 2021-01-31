ad19c1d89afbac735d44c1c9ad45988daa579f79
Duc Tran <duc.hoai@autonomous.nyc>
2021-01-31 17:43:13 +0700

    comment getRoomLIstUI

--- src/app/room/room.component.ts
+++ src/app/room/room.component.ts
@@ -70,7 +70,9 @@ export class RoomComponent implements OnInit, OnDestroy {
   }
 
   getRoomListUI() {
-    const list = this.roomService.getRoomList().filter((room) => room.peer[0] !== this.identity).sort((a, b) => b.peer.length - a.peer.length);
+    const list = this.roomService.getRoomList()
+      // .filter((room) => room.peer[0] !== this.identity)
+      .sort((a, b) => b.peer.length - a.peer.length);
     (window as any).roomListUI = list;
     return list;
   }
