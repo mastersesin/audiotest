7955b21f7ffe35d33852d7b2e1e072bc68cd5f76
Duc Tran <duc.hoai@autonomous.nyc>
2021-01-31 15:36:32 +0700

    build new production

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 npm run build
 git add .
 git commit -m "build new production"
-git push origin before-vuong-s-change
-git push google before-vuong-s-change
+git push origin beta-version
+git push google beta-version
